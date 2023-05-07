29634e73485ef485d65cb351b36253201e6de98f
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-05-07 10:38:58 +0100

    Add instructions.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! Blinks the LED on a Pico board
 //!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+//! This will blink an LED attached to GP2
 #![no_std]
 #![no_main]
 
@@ -53,12 +53,7 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
-    let mut led_pin = pins.led.into_push_pull_output();
+    let mut led_pin = pins.gpio2.into_push_pull_output();
 
     loop {
         info!("on!");
@@ -69,5 +64,3 @@ fn main() -> ! {
         delay.delay_ms(500);
     }
 }
-
-// End of file
